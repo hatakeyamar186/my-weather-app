410bb8bcb0dcb917d3f0d6db454775d6741c7d7b
Hatakeyama Ryuku <b1022186@fun.ac.jp>
2025-06-09 16:15:11 +0900

    デプロイ作業

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import path from 'path'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/my-weather-app/',
   resolve: {
     alias: {
      '@': path.resolve(path.dirname(new URL(import.meta.url).pathname), './src'),
